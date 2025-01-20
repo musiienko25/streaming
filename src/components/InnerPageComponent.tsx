9d180ceee8e654050661c05a692e4360e3678ae8
musiienko25 <devmusiienko@gmail.com>
2025-01-20 16:56:55 +0200

    added bacl

---
+++ src/components/InnerPageComponent.tsx
@@ -0,0 +1,10 @@
+const InnerPageComponent: React.FC = () => {
+    return (
+        <div>
+            <h1>Внутрішня Сторінка</h1>
+            <p>Це ваш перший компонент внутрішньої сторінки.</p>
+        </div>
+    );
+};
+
+export default InnerPageComponent;
